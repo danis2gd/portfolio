6725ca5726ba749eb266c9eaabdeae7117d35840
Daniel Chadwick <danielchadwick@Daniels-MBP.lan>
2020-03-22 12:53:53 +0000

    PORT webpack

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+import '../less/main.less';
+
+import $ from 'jquery';
+import typed from 'typed.js/lib/typed';
+import 'bootstrap';
+import 'popper.js';
+
+// globals
+window.jQuery = $;
+window.$ = $;
+window.Typed = typed;
