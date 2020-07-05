b204b1af0ce9073b613f0d5e9f52578172312210
Daniel Chadwick <djchadwick@outlook.com>
2020-07-05 20:13:30 +0100

    PORT changing webpack config

--- assets/js/inc/router.js
+++ assets/js/inc/router.js
@@ -1,4 +1,4 @@
-const routes = require('../../../web/js/fos_js_routes.json');
+const routes = require('../../../public/js/fos_js_routes.json');
 const Routing = require('../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js');
 
 Routing.setRoutingData(routes);
