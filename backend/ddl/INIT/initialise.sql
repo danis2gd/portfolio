25acabe4ca9d5079feffcce951b3c3ded31aeb67
Daniel Chadwick <djchadwick@outlook.com>
2021-05-10 22:08:23 +0100

    PORT backend - eslint/prettierc, initialise sql, mikro-orm naming strategy, Card (id fieldName)

---
+++ backend/ddl/INIT/initialise.sql
@@ -0,0 +1,10 @@
+INSERT INTO public."Card"
+    ("cardType", "title", "description", "displayOrder", "subTitle", "imagePath")
+VALUES
+    ('SKILL', 'PHP7', '<p></p>', 1, null, null),
+    ('SKILL', 'NodeJS', '<p></p>', 2, null, null),
+    ('WORK', 'Routing Optimisation System', '<p></p>', 1, null, null),
+    ('WORK', 'events', '<p></p>', 2, null, null),
+    ('WORK', 'Brewery Management System', '<p></p>', 3, null, null),
+    ('WORK', 'Business Dashboard', '<p></p>', 4, '(modash)', null)
+;
--- backend/src/entity/Card.ts
+++ backend/src/entity/Card.ts
@@ -3,8 +3,11 @@ import { CardType } from "../enum/CardType";
 import { Technology } from "../enum/Technology";
 
 @Entity()
+@Unique({ properties: ["cardType", "displayOrder"] })
 export class Card extends BaseEntity<Card, "_id"> {
-    @PrimaryKey()
+    @PrimaryKey({
+        fieldName: "id",
+    })
     _id!: number;
 
     @Enum(() => CardType)
@@ -14,17 +17,20 @@ export class Card extends BaseEntity<Card, "_id"> {
     title!: string;
 
     @Property()
-    subTitle!: string;
-
-    @Property()
-    imagePath!: string;
-
-    @Property()
     description!: string;
 
     @Property()
-    @Unique()
     displayOrder!: number;
+
+    @Property({
+        nullable: true,
+    })
+    subTitle?: string;
+
+    @Property({
+        nullable: true,
+    })
+    imagePath?: string;
 
     @Enum({ items: () => Technology, array: true, default: [] })
     technologies: Technology[];
